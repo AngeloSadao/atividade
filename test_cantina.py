6bca0e3f48ae427273101dcf18c40db89282ff94
Sadao <angelosadao321@gmail.com>
2026-03-23 17:14:02 -0300

    atividade_avaliativa

--- test_cantina.py
+++ test_cantina.py
@@ -1,4 +1,16 @@
 from calculadora import calcular_total, validar_pedido
 
-def test_calcular_unidade():
+def test_calcular_valor_itens():
     assert calcular_total(4, 2) == 8
+
+def test_erro_calcular_valor_itens():
+    assert calcular_total(4, 4) == 8
+
+def test_erro_calcular_valor_itens():
+    assert calcular_total(4, 3) == 8
+
+def test_validar_pedido_valor_itens():
+    assert validar_pedido("teste", 4, 2) == 8
+
+def test_erro_validar_pedido_valor_itens():
+    assert validar_pedido("teste", 4, 4) == 8
